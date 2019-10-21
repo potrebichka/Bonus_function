ae11d75b2ff2ee6dec66dc4b59120fa56584714d
Nina Potrebich and Sacide Basol <nina@>
2019-10-21 16:37:53 -0700

    add jquery images

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,40 @@ var anotherFunction = function(sentence) {
 }
 
 //console.log(anotherFunction(sentence))
-
-var thirdFunction = function(sentence) {
-  var newStr = someFunction(sentence);
-  var secondStr = anotherFunction(newStr)
-  return sentence + secondStr;
-}
-
-//console.log(thirdFunction(sentence));
-
 function fourthFunction (sentence) {
 
   var sentenceLength= sentence.length;
   var newNumber= sentenceLength/2;
   return sentence.charAt(newNumber);
+}
+//console.log(thirdFunction(sentence));
 
+var thirdFunction = function(sentence) {
+  var newStr = someFunction(sentence);
+  var secondStr = anotherFunction(newStr)
+  var thirdStr = fourthFunction (sentence);
+  var resultStr = thirdStr + sentence + secondStr;
+  /*
+  //variant1
+  var arrayStr = Array.from(resultStr).reverse();
+  return arrayStr.join('');
+
+  //variant2
+  var resultStrReversed = "";
+  for (let i=resultStr.length-1; i >= 0; i--) {
+      resultStrReversed += resultStr.charAt(i)
+  }
+    return resultStrReversed
+  */
+  return resultStr
 }
 
-console.log(fourthFunction(sentence))
+console.log(thirdFunction(sentence))
+
+jQuery("#img1").click(function() {
+  alert(sentence)
+});
+
+jQuery('#img2').click(function() {
+  alert(thirdFunction(sentence))
+})
