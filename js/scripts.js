e0d64fa0f5012f550e8580413c66ea15b95b9954
Nina Potrebich and Sacide Basol <nina@>
2019-10-21 16:04:06 -0700

    add some functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,31 @@ var sentence = prompt("Enter a sentence: ");
 console.log(sentence);
 
 var someFunction = function(sentence) {
-  return (sentence.slice(0,1) + sentence.slice(-1)).toUpperCase()
+  return (sentence.charAt(0) + sentence.charAt(sentence.length-1)).toUpperCase()
 }
 
-console.log(someFunction(sentence));
+//console.log(someFunction(sentence));
 
 var anotherFunction = function(sentence) {
-  return someFunction(sentence).slice(1,2)+someFunction(sentence).slice(0,1)
+  return (sentence.charAt(sentence.length-1) + sentence.charAt(0)).toUpperCase()
 }
 
-console.log(anotherFunction(sentence))
+//console.log(anotherFunction(sentence))
+
+var thirdFunction = function(sentence) {
+  var newStr = someFunction(sentence);
+  var secondStr = anotherFunction(newStr)
+  return sentence + secondStr;
+}
+
+//console.log(thirdFunction(sentence));
+
+function fourthFunction (sentence) {
+
+  var sentenceLength= sentence.length;
+  var newNumber= sentenceLength/2;
+  return sentence.charAt(newNumber);
+
+}
+
+console.log(fourthFunction(sentence))
